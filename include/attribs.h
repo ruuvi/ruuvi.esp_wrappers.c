f8bc2f51bc3fa80c033ff70c0b9132aadad473dd
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-12-11 22:57:32 +0700

    attribs.h: Add gcc function attributes 'alloc_size', 'returns_nonnull', 'warn_unused_result', 'const' and 'pure'

--- include/attribs.h
+++ include/attribs.h
@@ -18,13 +18,17 @@ extern "C" {
 #define ATTRIBUTE(attrs)
 #endif
 
-#define ATTR_NORETURN ATTRIBUTE((noreturn))
-#define ATTR_UNUSED   ATTRIBUTE((unused))
-#define ATTR_MALLOC   ATTRIBUTE((malloc))
-
+#define ATTR_NORETURN                  ATTRIBUTE((noreturn))
+#define ATTR_UNUSED                    ATTRIBUTE((unused))
+#define ATTR_MALLOC                    ATTRIBUTE((malloc))
+#define ATTR_MALLOC_SIZE(idx)          ATTRIBUTE((alloc_size(idx)))
+#define ATTR_CALLOC_SIZE(nmemb, size)  ATTRIBUTE((alloc_size(nmemb, size)))
+#define ATTR_RETURNS_NONNULL           ATTRIBUTE((returns_nonnull))
+#define ATTR_CONST                     ATTRIBUTE((const))
+#define ATTR_PURE                      ATTRIBUTE((pure))
+#define ATTR_WARN_UNUSED_RESULT        ATTRIBUTE((warn_unused_result))
 #define ATTR_PRINTF(idx_fmt, idx_args) ATTRIBUTE((format(printf, idx_fmt, idx_args)))
-
-#define ATTR_NONNULL(arg_idx, ...) ATTRIBUTE((nonnull(arg_idx, ##__VA_ARGS__)))
+#define ATTR_NONNULL(arg_idx, ...)     ATTRIBUTE((nonnull(arg_idx, ##__VA_ARGS__)))
 
 #ifdef __cplusplus
 }
