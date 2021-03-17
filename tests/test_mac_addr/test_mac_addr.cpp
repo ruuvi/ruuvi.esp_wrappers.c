57e79ceb2645d22e16fc4f4de4703a12eff29099
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2021-03-17 17:01:56 +0700

    mac_addr: add MAC_ADDR_INIT macro

--- tests/test_mac_addr/test_mac_addr.cpp
+++ tests/test_mac_addr/test_mac_addr.cpp
@@ -46,9 +46,9 @@ TestMacAddr::~TestMacAddr() = default;
 
 TEST_F(TestMacAddr, test_mac_address_bin_init) // NOLINT
 {
-    mac_address_bin_t mac_bin                    = { 0 };
-    const uint8_t     mac[MAC_ADDRESS_NUM_BYTES] = { 0x11U, 0x22U, 0x33U, 0xAAU, 0xBBU, 0xCCU };
-    mac_address_bin_init(&mac_bin, mac);
+    const std::array<uint8_t, MAC_ADDRESS_NUM_BYTES> mac     = { 0x11U, 0x22U, 0x33U, 0xAAU, 0xBBU, 0xCCU };
+    mac_address_bin_t                                mac_bin = { 0 };
+    mac_address_bin_init(&mac_bin, mac.data());
     ASSERT_EQ(mac_bin.mac[0], mac[0]);
     ASSERT_EQ(mac_bin.mac[1], mac[1]);
     ASSERT_EQ(mac_bin.mac[2], mac[2]);
@@ -57,11 +57,20 @@ TEST_F(TestMacAddr, test_mac_address_bin_init) // NOLINT
     ASSERT_EQ(mac_bin.mac[5], mac[5]);
 }
 
+TEST_F(TestMacAddr, test_mac_address_init_macro) // NOLINT
+{
+    const mac_address_bin_t mac_bin = MAC_ADDR_INIT(0x11U, 0x22U, 0x33U, 0xAAU, 0xBBU, 0xCCU);
+    ASSERT_EQ(mac_bin.mac[0], 0x11);
+    ASSERT_EQ(mac_bin.mac[1], 0x22);
+    ASSERT_EQ(mac_bin.mac[2], 0x33);
+    ASSERT_EQ(mac_bin.mac[3], 0xAA);
+    ASSERT_EQ(mac_bin.mac[4], 0xBB);
+    ASSERT_EQ(mac_bin.mac[5], 0xCC);
+}
+
 TEST_F(TestMacAddr, test_mac_address_to_str) // NOLINT
 {
-    const uint8_t     mac[MAC_ADDRESS_NUM_BYTES] = { 0x11U, 0x22U, 0x33U, 0xAAU, 0xBBU, 0xCCU };
-    mac_address_bin_t mac_bin                    = { 0 };
-    mac_address_bin_init(&mac_bin, mac);
+    const mac_address_bin_t mac_bin = MAC_ADDR_INIT(0x11U, 0x22U, 0x33U, 0xAAU, 0xBBU, 0xCCU);
     const mac_address_str_t mac_str = mac_address_to_str(&mac_bin);
     ASSERT_EQ(string("11:22:33:AA:BB:CC"), string(mac_str.str_buf));
 }
