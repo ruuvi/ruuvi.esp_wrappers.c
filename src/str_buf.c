ab094992d02a3709e973707811d55f977817fa73
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2021-05-14 13:19:37 +0700

    str_buf: Add API str_buf_bin_to_hex and str_buf_bin_to_hex_with_alloc

--- src/str_buf.c
+++ src/str_buf.c
@@ -8,6 +8,7 @@
 #include "str_buf.h"
 #include <stdio.h>
 #include "os_malloc.h"
+#include "attribs.h"
 
 ATTR_NONNULL(1)
 str_buf_t
@@ -158,6 +159,100 @@ str_buf_printf_with_alloc(const char *const fmt, ...)
     va_start(args, fmt);
     const str_buf_t str_buf = str_buf_vprintf_with_alloc(fmt, args);
     va_end(args);
+    return str_buf;
+}
+
+ATTR_NONNULL(1, 2)
+bool
+str_buf_bin_to_hex(str_buf_t *const p_str_buf, const uint8_t *const p_input_buf, const size_t input_buf_size)
+{
+    static const char hex[] = "0123456789abcdef";
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnonnull-compare"
+    if (NULL == p_str_buf)
+    {
+        return false;
+    }
+#pragma GCC diagnostic pop
+    if ((NULL == p_str_buf->buf) && (0 != p_str_buf->size))
+    {
+        return false;
+    }
+    if ((0 == p_str_buf->size) && (NULL != p_str_buf->buf))
+    {
+        return false;
+    }
+    if (0 != p_str_buf->size)
+    {
+        if (p_str_buf->idx >= p_str_buf->size)
+        {
+            return false;
+        }
+    }
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnonnull-compare"
+    if (NULL == p_input_buf)
+    {
+        return false;
+    }
+#pragma GCC diagnostic pop
+    char *       p_buf   = (NULL != p_str_buf->buf) ? &p_str_buf->buf[p_str_buf->idx] : NULL;
+    const size_t max_len = (0 != p_str_buf->size) ? (p_str_buf->size - p_str_buf->idx) : 0;
+
+    bool flag_overflow = false;
+    size_t len = 0;
+    if (NULL == p_buf)
+    {
+        len = input_buf_size * 2;
+    }
+    else
+    {
+        for (size_t i = 0; i < input_buf_size; ++i)
+        {
+            const uint8_t byte_val = p_input_buf[i];
+
+            if ((len + 2) >= max_len)
+            {
+                flag_overflow = true;
+                break;
+            }
+            p_buf[len++] = hex[(uint8_t)(byte_val >> 4U) & 0x0FU];
+            p_buf[len++] = hex[byte_val & 0x0FU];
+        }
+        p_buf[len] = '\0';
+    }
+
+    p_str_buf->idx += len;
+    if (0 != p_str_buf->size)
+    {
+        if (p_str_buf->idx >= p_str_buf->size)
+        {
+            p_str_buf->idx = p_str_buf->size;
+            return false;
+        }
+        if (flag_overflow)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ATTR_NONNULL(1)
+str_buf_t
+str_buf_bin_to_hex_with_alloc(const uint8_t *const p_input_buf, const size_t input_buf_size)
+{
+    str_buf_t str_buf = str_buf_init_null();
+    const bool res = str_buf_bin_to_hex(&str_buf, p_input_buf, input_buf_size);
+    if (!res)
+    {
+        return str_buf_init_null();
+    }
+    if (!str_buf_init_with_alloc(&str_buf))
+    {
+        return str_buf_init_null();
+    }
+    str_buf_bin_to_hex(&str_buf, p_input_buf, input_buf_size);
     return str_buf;
 }
 
